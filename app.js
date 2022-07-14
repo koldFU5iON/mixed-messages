3361e401ddda0c294c8789d684045c50bd392bd2
Devon Stanton <devon.stanton@gmail.com>
2022-07-14 11:13:48 +0200

    changed messages to an object and moved function to messages, app now calls function only

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
 import { messages } from "./messages.js";
 
-const rIndex = () => {
-    return Math.floor(Math.random() * messages.length)
-}
-
-console.log(messages[rIndex()])
+console.log(messages.create())
--- messages.js
+++ messages.js
@@ -1,4 +1,18 @@
-export const messages = [
-    "Hello World!",
-    "Another Message"
-]
+export const messages = {
+    _intro: [
+        "You are a"
+        , "You have a"
+        , "You will a"
+    ],
+    _adjcetives: [
+        "alive", "better", "careful", "clever", "dead", "easy", "famous", "gifted", "hallowed", "helpful", "important", "inexpensive", "mealy", "mushy", "odd", "poor", "powerful", "rich", "shy", "tender",
+        "unimportant", "uninterested", "vast", "wrong"
+    ],
+    _objects: ["Pen","Computer", "Notebook", "Desk", "Pencil", "Bookcase", "Book", "Chair", "Backpack", "Paper", "Glue", "Door", "Ruler", "Clock", "Whiteboard", "Window"],
+    _rIndex(words) {
+        return Math.floor(Math.random() * words.length)
+    },
+    create() {
+        return `${this._intro[this._rIndex(this._intro)]} ${this._adjcetives[this._rIndex(this._adjcetives)]} ${this._objects[this._rIndex(this._objects)]}!`
+    }
+}
